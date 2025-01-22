2e1fa91372d210389ce453676f32776d59e769b4
amin063 <aminaliyev630@gmail.com>
2025-01-22 12:24:46 +0400

    Nothing)

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 
 const database = () => {
-    
     mongoose.connect(process.env.MONGO_URI).then(() => console.log("Baglanti basarili"))
         .catch((err) => console.log("asdfasd" + err))
-
 }
 
 
