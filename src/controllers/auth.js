020da0b4ef8e27fe6be34e9985086bd072685881
amin063 <aminaliyev630@gmail.com>
2025-01-22 20:29:47 +0400

    change cookie code

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -53,7 +53,7 @@ const login = async (req, res) => {
         );
         
         res.cookie('token', token, {
-            httpOnly: true,    
+            httpOnly: false,    
             secure: true,     
             sameSite: 'strict',
             maxAge: 3600000  
