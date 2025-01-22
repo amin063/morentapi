85dbcd0ec49cc0adf0adfeb0056de8518ba54686
amin063 <aminaliyev630@gmail.com>
2025-01-22 20:58:49 +0400

    Add authentication middleware to protect routes

---
+++ src/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+const auth = (req, res, next) => {
+    const token = req.cookies.token
+
+    if (!token) {
+        return res.status(401).json({ msg: 'Token yoxdur' })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+    } catch (error) {
+        return res.status(401).json({ message: 'Token etibarsızdır' })
+    }
+}
+
+module.exports = auth
--- src/routes/car.js
+++ src/routes/car.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router();
-const {addCar, getCars, getCarDetails, filterCars, deleteCar} = require('../controllers/car.js')
+const {addCar, getCars, getCarDetails, filterCars, deleteCar} = require('../controllers/car.js');
+const auth = require('../middleware/auth.js');
 
-router.post('/add', addCar)
-router.get('/cars', getCars)
-router.get('/details/:id', getCarDetails)
-router.get('/filter', filterCars)
-router.delete('/delete/:id', deleteCar)
+router.post('/add', auth , addCar)
+router.get('/cars', auth , getCars)
+router.get('/details/:id', auth , getCarDetails)
+router.get('/filter', auth , filterCars)
+router.delete('/delete/:id', auth , deleteCar)
 
 module.exports = router
