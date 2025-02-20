20c0358acbc48423e0a4de34867cfdcd769cd5af
amin063 <aminaliyev630@gmail.com>
2025-02-20 16:49:48 +0400

    Changed origin url

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ db()
 
 const app = express()
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://morent-steel.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
