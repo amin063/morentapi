8440b59f699077490697d3559e0e69feeb0c9b0e
amin063 <aminaliyev630@gmail.com>
2025-02-20 15:38:12 +0400

    Changed sameSite

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -58,7 +58,7 @@ const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'None',
             maxAge: 3600000 // 1 hour
         })
 
