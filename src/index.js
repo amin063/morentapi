4283f3e7b514bed639c3db1360f1f8bfd75900e4
amin063 <aminaliyev630@gmail.com>
2025-04-13 14:26:09 +0400

    Update

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ db()
 
 const app = express()
 const corsOptions = {
-    origin: '*',
+    // origin: 'https://morent-steel.vercel.app',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
