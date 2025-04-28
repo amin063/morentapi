58de2a26e37eadb43a8a7f028d95f2b508827eba
FeriW02 <full.stack020224@gmail.com>
2025-04-29 01:49:51 +0400

    asdfgf

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ db()
 
 const app = express()
 const corsOptions = {
-    // origin: 'https://morent-steel.vercel.app',
-    origin: 'http://localhost:5173',
+    origin: 'https://morent-steel.vercel.app',
+    // origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
